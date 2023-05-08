8fa3092bc9566641e44deed5cd9c638a2b7ba0ed
Javier Blasco <blascojavi@gmail.com>
2023-05-08 17:45:36 +0200

    Act 08-05-2023

--- app/src/main/java/com/jablal/proyectojavierblasco/ConfigActivity.java
+++ app/src/main/java/com/jablal/proyectojavierblasco/ConfigActivity.java
@@ -4,32 +4,32 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.graphics.Camera;
 import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
 
-import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+
+
 
 public class ConfigActivity extends AppCompatActivity {
     private Spinner spinner;
@@ -37,18 +37,19 @@ public class ConfigActivity extends AppCompatActivity {
     private TextView textViewUser;
     public String newName = "";
     SharedPreferences sharedPref;
-
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getSupportActionBar().hide();//ocultamos la barra superior, dejando mas pantalla libre
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
         textViewNameUser = findViewById(R.id.textViewNameUser);
         textViewUser = findViewById(R.id.textViewUser);
+            ///
 
-
+            ///
         ImageButton boton = findViewById(R.id.imageButtonHome2);
         boton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,13 +58,10 @@ public class ConfigActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-            //indicamos la imagen por defecto del usuario
-        ImageView imageView2 = findViewById(R.id.imageView2);
+
+        ImageView imageView2 = findViewById(R.id.image_View);
         imageView2.setImageResource(R.drawable.image_user);
 
-
-
-        // Inicializar sharedPref dentro del método onCreate()
         sharedPref = getSharedPreferences("myPref", Context.MODE_PRIVATE);
 
         Button botonIdioma = findViewById(R.id.buttonSelectedIdiom);
@@ -74,38 +72,27 @@ public class ConfigActivity extends AppCompatActivity {
             }
         });
 
-
-        // Obtener una referencia al Spinner en tu layout XML
         spinner = findViewById(R.id.spinner_idiomas);
 
-        // Crear una lista de idiomas
         List<String> idiomas = new ArrayList<>();
         idiomas.add(getString(R.string.idioma_espanol));
         idiomas.add(getString(R.string.idioma_ingles));
 
         spinner.setBackgroundColor(getResources().getColor(R.color.teal_200));
 
-        // Crear un ArrayAdapter que se encargará de vincular los datos con la vista de cada elemento
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(ConfigActivity.this, android.R.layout.simple_spinner_item, idiomas);
-
-        // Especificar la apariencia del Spinner cuando se muestra la lista de opciones
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // Asignar el adaptador al Spinner
         spinner.setAdapter(adapter);
-        // Agregar un Listener al Spinner para detectar cuando se selecciona un idioma
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String selectedLanguage = (String) parent.getItemAtPosition(position);
                 selectedLanguage=spinner.getSelectedItem().toString();
-
                 if (selectedLanguage.equals(getString(R.string.idioma_espanol))) {
                     cambiarIdioma("es");
                 } else if (selectedLanguage.equals(getString(R.string.idioma_ingles))) {
                     cambiarIdioma("en");
                 }
-
             }
 
             @Override
@@ -114,31 +101,34 @@ public class ConfigActivity extends AppCompatActivity {
         });
 
         spinner.setSelection(0);
-        // Obtener el texto del elemento seleccionado
-        String selectedText = spinner.getSelectedItem().toString();
 
-        // Crear un SpannableString para el texto seleccionado y establecer su color a rojo
-        //SpannableString spannableString = new SpannableString(selectedText);
-        //spannableString.setSpan(new ForegroundColorSpan(Color.RED), 0, selectedText.length(), 0);
-        // Obtiene la vista del elemento seleccionado en el Spinner
         View selectedItemView = spinner.getSelectedView();
-
-        // Si la vista es un TextView, establece el color del texto a negro
         if (selectedItemView instanceof TextView) {
             ((TextView) selectedItemView).setTextColor(Color.BLACK);
         }
 
-
-
-
         String defaultValue = getResources().getString(R.string.name_user);
         String nameUser = sharedPref.getString("name_user", defaultValue);
-
 
         textViewNameUser.setText(nameUser);
         textViewUser.setText(nameUser);
 
-        /////
+
+
+        Button buttonChangePicture = findViewById(R.id.button_change_picture);
+        buttonChangePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+
+
+
+            }
+        });
 
 
 
@@ -151,11 +141,6 @@ public class ConfigActivity extends AppCompatActivity {
 
 
 
-
-
-
-
-        /////
 
     }
 
@@ -180,14 +165,10 @@ public class ConfigActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        // Obtener el texto del TextView y guardarlo en la variable newName
-        newName = textViewNameUser.getText().toString();
 
-        // Obtener una referencia al EditText
-        EditText editText = findViewById(R.id.pruebaText);
 
-        // Sobrescribir el texto del EditText con el valor de la variable newName
-        editText.setText(newName);
+
+
     }
 
 
@@ -215,8 +196,17 @@ public class ConfigActivity extends AppCompatActivity {
 
     }
 
+/////
 
 
 
 
+
+
+
+
+
+
+
+/////
 }
--- app/src/main/java/com/jablal/proyectojavierblasco/HouseActivity2.java
+++ app/src/main/java/com/jablal/proyectojavierblasco/HouseActivity2.java
@@ -3,14 +3,24 @@ package com.jablal.proyectojavierblasco;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class HouseActivity2 extends AppCompatActivity {
 
 
 
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Bitmap imageBitmap;
 
 
     @Override
@@ -32,5 +42,39 @@ public class HouseActivity2 extends AppCompatActivity {
 
 
     }
+
+    public void capturePhoto(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            saveImageToExternalStorage(imageBitmap);
+            Toast.makeText(this, "Foto capturada y guardada", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void saveImageToExternalStorage(Bitmap bitmap) {
+        String path = Environment.getExternalStorageDirectory().toString();
+        File file = new File(path, "photo.jpg");
+        FileOutputStream fos;
+        try {
+            fos = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.flush();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
