3b59fdb9461bb7a28da8d46a7548dad27c8ced5f
Javier Blasco <blascojavi@gmail.com>
2023-05-24 19:38:17 +0200

    audios ActivityWc

--- app/src/main/java/com/jablal/proyectojavierblasco/ActivityWc.java
+++ app/src/main/java/com/jablal/proyectojavierblasco/ActivityWc.java
@@ -9,12 +9,38 @@ import android.widget.ImageButton;
 
 public class ActivityWc extends AppCompatActivity {
 
+    private ImageButton imageButtonNoQuedaPapel,imageButtondameLaToalla,imageButtoncortarLasUnyas,imageButtonLimpiarElCulo,imageButtonQuieroJuguetesDeBanyo,imageButtonHaceCaca,imageButtonLavarmeLosDientes,
+    imageButtonNecesitoIntimidad,imageButtonCepillarmeElPelo,imageButtonEscobilla,imageButtonMeHeHechoPipi,imageButtonSecarmeElCuerpo,imageButtonLavarLaCara,imageButtonSecarseElPelo,
+    imageButtonTengoGanas,imageButtonLimpiarElPis,imageButtonPeinarme,imageButtonLavarseLasManos2,imageButtonQuieroDucharme,imageButtonquieroBanyarme,imageButtonacerPis;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getSupportActionBar().hide();//ocultamos la barra superior, dejando mas pantalla libre
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wc);
 
+        imageButtonNoQuedaPapel = findViewById(R.id.imageButtonNoQuedaPapel);
+        imageButtondameLaToalla = findViewById(R.id.imageButtondameLaToalla);
+        imageButtoncortarLasUnyas = findViewById(R.id.imageButtoncortarLasUnyas);
+        imageButtonLimpiarElCulo = findViewById(R.id.imageButtonLimpiarElCulo);
+        imageButtonQuieroJuguetesDeBanyo = findViewById(R.id.imageButtonQuieroJuguetesDeBanyo);
+        imageButtonHaceCaca = findViewById(R.id.imageButtonHaceCaca);
+        imageButtonLavarmeLosDientes = findViewById(R.id.imageButtonLavarmeLosDientes);
+        imageButtonNecesitoIntimidad = findViewById(R.id.imageButtonNecesitoIntimidad);
+        imageButtonCepillarmeElPelo = findViewById(R.id.imageButtonCepillarmeElPelo);
+        imageButtonEscobilla = findViewById(R.id.imageButtonEscobilla);
+        imageButtonMeHeHechoPipi = findViewById(R.id.imageButtonMeHeHechoPipi);
+        imageButtonSecarmeElCuerpo = findViewById(R.id.imageButtonSecarmeElCuerpo);
+        imageButtonLavarLaCara = findViewById(R.id.imageButtonLavarLaCara);
+        imageButtonSecarseElPelo = findViewById(R.id.imageButtonSecarseElPelo);
+        imageButtonTengoGanas = findViewById(R.id.imageButtonTengoGanas);
+        imageButtonLimpiarElPis = findViewById(R.id.imageButtonLimpiarElPis);
+        imageButtonPeinarme = findViewById(R.id.imageButtonPeinarme);
+        imageButtonLavarseLasManos2 = findViewById(R.id.imageButtonLavarseLasManos2);
+        imageButtonQuieroDucharme = findViewById(R.id.imageButtonQuieroDucharme);
+        imageButtonquieroBanyarme = findViewById(R.id.imageButtonquieroBanyarme);
+        imageButtonacerPis = findViewById(R.id.imageButtonacerPis);
 
 
 
@@ -26,6 +52,9 @@ public class ActivityWc extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+
 
 
 
